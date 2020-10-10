7370ab362f315d1d9468db8c70c99b4bb2f5a3d0
Nyarlana <52347671+Nyarlana@users.noreply.github.com>
2020-10-10 16:33:00 +0200

    Inheritance TileMap

--- src/TileMap/TileMap.h
+++ src/TileMap/TileMap.h
@@ -1,15 +1,22 @@
 #ifndef TILEMAP_H
 #define TILEMAP_H
 #include "FileReader.h"
+#include "../gameManager/Component.h"
+#include "../gameManager/Observer.h"
 #include "Tile.h"
 
-class TileMap
+class TileMap : public Component, public Observer, public Subject
 {
     public:
         TileMap();
         ~TileMap();
         void setTab();
         void printTab();
+        // Surcharge
+        virtual void on_Notify(const Component& subject, Event event);
+        virtual void _init();
+        virtual void _update();
+        virtual void _draw(sf::RenderWindow & window);
 
     protected:
 
