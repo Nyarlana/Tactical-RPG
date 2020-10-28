69262b040e4008c67416e4b94e9e2196eaa60e5f
WhyLxx <wyatt.leaux@gmail.com>
2020-10-28 14:47:21 +0100

    nouveau test

--- src/TileMap/FileReader.cpp
+++ src/TileMap/FileReader.cpp
@@ -19,13 +19,7 @@ void FileReader::readFile(char tab[TM_X_TAB][TM_Y_TAB])
 {
     string line;
     int line_index=0;
-    if(!ifstream tilemap_file ("data/tilemap.txt"))
-    {
-        if(!ifstream tilemap_file ("../data/tilemap.txt"))
-        {
-            cout<<"unable to open file";
-        } 
-    }
+    ifstream tilemap_file ("data/tilemap.txt");
     if (tilemap_file.is_open())
     {
         while (getline (tilemap_file,line))
@@ -37,6 +31,26 @@ void FileReader::readFile(char tab[TM_X_TAB][TM_Y_TAB])
             line_index++;
         }
         tilemap_file.close();
+    }
+    else
+    {
+        ifstream tilemap_file2 ("../data/tilemap.txt");
+        if (tilemap_file2.is_open())
+        {
+            while (getline (tilemap_file2,line))
+            {
+                for(int i=0;i<TM_X_TAB;++i)
+                {
+                    tab[line_index][i]=line[i];
+                }
+                line_index++;
+            }
+            tilemap_file2.close();
+        }
+        else
+        {
+            cout<<"unable to open file";
+        }
     }
 }
 
