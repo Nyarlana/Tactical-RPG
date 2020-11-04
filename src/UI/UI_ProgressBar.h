f3a16a8218394ac643e45cbfe252c665d9d1616a
Nyarlana <52347671+Nyarlana@users.noreply.github.com>
2020-11-04 20:11:10 +0100

    comments

--- src/UI/UI_ProgressBar.h
+++ src/UI/UI_ProgressBar.h
@@ -10,7 +10,9 @@ public:
   @param pos position
   @param bar dimensions of the form
   @param max_value_ maximum value of the bar
-  @param value_ current value of the bar*/
+  @param value_ current value of the bar
+  @param bg_col color of back of bar
+  @param col color of front of bar*/
   UI_ProgressBar(sf::Vector2i pos, sf::Vector2i shape, int max_value_ = 100, int value_ = 100, sf::Color bg_col = sf::Color::Red, sf::Color col = sf::Color::Green);
   //surcharge
   virtual void _init();
